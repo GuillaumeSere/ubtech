965696a93e84d4fd222ad28f9632723c95e78d83
Guillaume Sere <guillaumesere@hotmail.fr>
2026-01-19 16:40:04 +0100

    add validation google

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,22 +7,37 @@ import Footer from '@/components/ui/Footer';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'UBTECH Robotics - Expérience 3D',
-  description: 'Découvrez les robots Walker et Cruzr en 3D interactif',
+    title: 'UBTECH Robotics - Expérience 3D',
+    description: 'Découvrez les robots Walker et Cruzr en 3D interactif,three.js, webgl, robot 3D, landing page 3D, ubtech, site immersif, 3D web,',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="fr">
-      <body className={inter.className}>
-        <Navbar /> 
-        {children}
-        <Footer /> 
-      </body>
-    </html>
-  );
+    return (
+        <html lang="fr">
+            <body className={inter.className}>
+                <head>
+                    <meta name="google-site-verification" content="nwVPqsKRGvHVh9v-Qn4QoawQzNbN99Sfg6usOSlUEhg" />
+                    <meta property="og:title" content="UBTECH Robot 3D – Landing Page Three.js" />
+                    <meta property="og:description" content="Une démo web immersive avec un robot 3D interactif réalisée en Three.js." />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:image" content="./home.jpg" />
+                    <meta property="og:url" content="https://ubtech-walker.vercel.app/" />
+                    <meta name="keywords" content="three.js, webgl, robot 3D, landing page 3D, ubtech, site immersif, 3D web, frontend, creative coding" />
+                    <meta name="author" content="Guillaume Tech" />
+
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:title" content="Robot 3D UBTECH – Three.js Demo" />
+                    <meta name="twitter:description" content="Landing page immersive avec un robot 3D en temps réel." />
+                    <meta name="twitter:image" content="./home.jpg" />
+                </head>
+                <Navbar />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
 }
