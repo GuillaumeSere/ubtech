6d671a2bab0d17bdc980ab1414ab71b34b53be5b
Guillaume Sere <guillaumesere@hotmail.fr>
2026-01-20 12:52:20 +0100

    modif

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,18 +21,18 @@ export default function RootLayout({
             <head>
                 <link href="https://fonts.googleapis.com/css2?family=Delius&family=Orbitron:wght@400..900&display=swap" rel="stylesheet"></link>
                 <meta name="google-site-verification" content="nwVPqsKRGvHVh9v-Qn4QoawQzNbN99Sfg6usOSlUEhg" />
-                <meta property="og:title" content="UBTECH Robot 3D – Landing Page Three.js" />
+                <meta property="og:title" content="UBTECH Robot 3D – Three.js" />
                 <meta property="og:description" content="Une démo web immersive avec un robot 3D interactif réalisée en Three.js." />
                 <meta property="og:type" content="website" />
-                <meta property="og:image" content="./home.jpg" />
+                <meta property="og:image" content="/images/home.jpg" />
                 <meta property="og:url" content="https://ubtech-walker.vercel.app/" />
                 <meta name="keywords" content="three.js, webgl, robot 3D, landing page 3D, ubtech, site immersif, 3D web, frontend, creative coding" />
                 <meta name="author" content="Guillaume Tech" />
 
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content="Robot 3D UBTECH – Three.js Demo" />
-                <meta name="twitter:description" content="Landing page immersive avec un robot 3D en temps réel." />
-                <meta name="twitter:image" content="./home.jpg" />
+                <meta name="twitter:description" content="Une démo web immersive avec un robot 3D interactif réalisée en Three.js." />
+                <meta name="twitter:image" content="/images/home.jpg" />
             </head>
             <body className={inter.className}>
                 <Navbar  />
